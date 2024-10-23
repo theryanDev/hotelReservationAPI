dafc9e76c13f83c6a56099d34e89509d33c16038
theryanDev <therlyson2017@gmail.com>
2024-10-22 23:19:54 -0300

    initial commit

---
+++ src/test/java/com/theryanDev/HotelReservationAPI/HotelReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theryanDev.HotelReservationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
